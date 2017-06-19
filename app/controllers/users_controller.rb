b28fd2330ffd0794e2cbbaf80b8dec7cfc6c5527
Zuhair A. Shaikh <Zuhair.A.Shaikh@outlook.com>
2017-06-19 15:54:38 -0700

    first routes and controllers finished

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,58 @@
+class UsersController < ApplicationController
+  def index
+    # render text: "Hi there, I am in the index action :D :P"
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    user = User.new(params[:user].permit(:name, :email))
+    if user.save
+      render json: user
+    else
+      render(
+        json: user.errors.full_messages, status: :unprocessable_entity
+      )
+    end
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    #user = User.new(params[:user].permit(:name, :email))
+    if @user
+      render json: @user
+    else
+      render json: ["could not find post"]
+    end
+    #render json: user
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      render json: @user
+    else
+      render json: @user.error.full_messages, status: 400
+    end
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    if @user
+      @user.destroy
+      render json: @user
+    else
+      render json: ["cant find cant delete"]
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:email)
+  end
+
+
+
+end
